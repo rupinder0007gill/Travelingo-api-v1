57706050ee696b2745b0bfe0eeae0ed430466d7b
Arvind <arvind.yuvasoft@gmail.com>
2018-12-20 12:51:29 +0530

    add extra parameters for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token  
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  respond_to :json
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob, :mobile_phone, :avatar])
+  end
 end
